c229aa0aa7dd75c1ee625d8e7a3455aaaa8e3598
root <root@dev-dsk-zhyilun-2b-613cdb61.us-west-2.amazon.com>
2022-10-07 22:58:25 +0800

    Init solution for electronic clock

--- sims/verilator/run_prog/obj_dir/Vsimu_top__Dpi.d
+++
@@ -1,7 +0,0 @@
-Vsimu_top__Dpi.o: Vsimu_top__Dpi.cpp Vsimu_top__Dpi.h \
- /usr/share/verilator/include/vltstd/svdpi.h Vsimu_top.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_save.h \
- /usr/share/verilator/include/verilated_heavy.h \
- /usr/share/verilator/include/verilated.h
